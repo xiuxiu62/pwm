f834129322fc28fecc807359c4830f5aa823e586
Justin Cremer <jacremer@live.com>
2021-11-13 16:18:22 -0800

    sensible key binds

--- src/keys.rs
+++ src/keys.rs
@@ -18,13 +18,14 @@ pub fn gen_key_map() -> KeyBindings<XcbConnection> {
         "M-p" => run_external!(LAUNCHER);
         "M-f" => run_external!(FILE_MANAGER);
         "M-w" => run_external!(BROWSER);
+        "M-e" => run_external!("emacs");
 
         // Client management
         "M-j" => run_internal!(cycle_client, Forward);
         "M-k" => run_internal!(cycle_client, Backward);
         "M-S-j" => run_internal!(drag_client, Forward);
         "M-S-k" => run_internal!(drag_client, Backward);
-        "M-S-q" => run_internal!(kill_client);
+        "M-S-c" => run_internal!(kill_client);
         "M-S-f" => run_internal!(toggle_client_fullscreen, &Selector::Focused);
 
         // Workspace management
@@ -37,13 +38,13 @@ pub fn gen_key_map() -> KeyBindings<XcbConnection> {
         // Layout management
         "M-space" => run_internal!(cycle_layout, Forward);
         "M-S-space" => run_internal!(cycle_layout, Backward);
-        "M-A-Up" => run_internal!(update_max_main, More);
-        "M-A-Down" => run_internal!(update_max_main, Less);
-        "M-A-Right" => run_internal!(update_main_ratio, More);
-        "M-A-Left" => run_internal!(update_main_ratio, Less);
+        "M-h" => run_internal!(update_max_main, More);
+        "M-l" => run_internal!(update_max_main, Less);
+        "M-S-h" => run_internal!(update_main_ratio, More);
+        "M-S-l" => run_internal!(update_main_ratio, Less);
 
         "M-A-s" => run_internal!(detect_screens);
-        "M-A-Escape" => run_internal!(exit);
+        "M-A-z" => run_internal!(exit);
 
         // Workspaces
         refmap [ 1..5 ] in {
